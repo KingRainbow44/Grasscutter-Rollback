843c93f9e8a0d508f8d1aac4bbc678b39b0eef4f
KingRainbow44 <kobedo11@gmail.com>
2023-05-05 18:59:46 -0400

    Scaffold plugin

---
+++ src/main/java/io/grasscutter/rollback/Rollback.java
@@ -0,0 +1,33 @@
+package io.grasscutter.rollback;
+
+import emu.grasscutter.plugin.Plugin;
+import lombok.Getter;
+import org.slf4j.Logger;
+
+public final class Rollback extends Plugin {
+    @Getter private static Rollback instance;
+
+    /**
+     * @return The plugin's logger.
+     */
+    public static Logger getPluginLogger() {
+        return Rollback.getInstance().getLogger();
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+
+        this.getLogger().info("Rollback has been loaded.");
+    }
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Rollback has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Rollback has been disabled.");
+    }
+}
